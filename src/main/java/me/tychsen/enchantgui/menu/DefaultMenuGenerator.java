2360e3f2c8f90e265294336a6887c0fe1ce040c5
sarhatabaot <sarhatabaot1@gmail.com>
2019-01-24 14:25:57 +0200

    Update DefaultMenuGenerator.java

--- src/main/java/me/tychsen/enchantgui/menu/DefaultMenuGenerator.java
+++ src/main/java/me/tychsen/enchantgui/menu/DefaultMenuGenerator.java
@@ -2,6 +2,7 @@ package me.tychsen.enchantgui.menu;
 
 import me.tychsen.enchantgui.config.EshopConfig;
 import me.tychsen.enchantgui.config.EshopEnchants;
+import me.tychsen.enchantgui.config.EshopShop;
 import me.tychsen.enchantgui.economy.NullPayment;
 import me.tychsen.enchantgui.Main;
 import me.tychsen.enchantgui.permissions.EshopPermissionSys;
@@ -13,6 +14,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +31,7 @@ public class DefaultMenuGenerator implements MenuGenerator {
         // TODO: Figure out better fix of EshopEnchants.
         this.enchants = new EshopEnchants();
         this.permsys = permsys;
+
     }
 
     public Inventory mainMenu(Player p) {
@@ -51,8 +54,14 @@ public class DefaultMenuGenerator implements MenuGenerator {
                 itemList.add(item);
             }
         }
-
         inv.setContents(itemList.toArray(new ItemStack[itemList.size()]));
+    }
+
+    private String format(int type, String name){
+        EshopShop shop = EshopShop.getInstance();
+        String string = shop.getString("shop."+name);
+        string = ChatColor.translateAlternateColorCodes('&',string);
+        return MessageFormat.format(string, type);
     }
 
     private Inventory generateEnchantMenu(Player p, ItemStack item, Map<String, String[]> playerLevels) {
@@ -75,12 +84,10 @@ public class DefaultMenuGenerator implements MenuGenerator {
                 ItemMeta meta = tmp.getItemMeta();
                 List<String> lores = new ArrayList<>();
 
-                //TODO: Option to change color, localization message here?
-                
-                lores.add(ChatColor.GOLD + "Level: " + level);
+                lores.add(format(level,"level"));
                 if (!(config.getEconomy() instanceof NullPayment)) {
                     int price = config.getPrice(enchantment, level);
-                    lores.add(ChatColor.GREEN + "Price: " + price);
+                    lores.add(format(price,"price"));
                 }
                 meta.setLore(lores);
                 tmp.setItemMeta(meta);
@@ -95,13 +102,18 @@ public class DefaultMenuGenerator implements MenuGenerator {
         inv.setContents(itemList.toArray(new ItemStack[itemList.size()]));
 
         // Generate and insert a back button
-        ItemStack backitem = new ItemStack(Material.EMERALD);
-        ItemMeta meta = backitem.getItemMeta();
-        meta.setDisplayName("Go back");
-        backitem.setItemMeta(meta);
-        inv.setItem(27, backitem);
+        inv.setItem(27, generateBackItem());
 
         playerLevels.put(p.getName(), levels.toArray(new String[levels.size()]));
         return inv;
+    }
+
+    private ItemStack generateBackItem(){
+        Material material = Material.matchMaterial(EshopShop.getInstance().getString("shop.back-item"));
+        ItemStack backItem = new ItemStack(material);
+        ItemMeta meta = backItem.getItemMeta();
+        meta.setDisplayName("Go back");
+        backItem.setItemMeta(meta);
+        return backItem;
     }
 }
