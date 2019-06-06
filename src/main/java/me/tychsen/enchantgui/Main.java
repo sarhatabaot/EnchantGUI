a5f40938469f6c546cdf5f084ad5413bd32cf1ae
Omer Oreg <sarhatabaot1@gmail.com>
2019-06-06 17:40:57 +0300

    Added check for vault

--- src/main/java/me/tychsen/enchantgui/Main.java
+++ src/main/java/me/tychsen/enchantgui/Main.java
@@ -3,17 +3,25 @@ package me.tychsen.enchantgui;
 import me.tychsen.enchantgui.config.EshopConfig;
 import me.tychsen.enchantgui.event.EventManager;
 import me.tychsen.enchantgui.menu.DefaultMenuSystem;
+import net.milkbowl.vault.economy.Economy;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
     private static Main instance;
+    private static Economy econ = null;
 
     @Override
     public void onEnable() {
         instance = this;
+        if (!setupEconomy() ) {
+            getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
         getLogger().info("Loading configs and stuff...");
 
         // Generate config.yml if there is none
@@ -34,6 +42,24 @@ public class Main extends JavaPlugin implements Listener {
     public static void debug(String msg){
         if(EshopConfig.getInstance().getDebug())
             Bukkit.getPluginManager().getPlugin("EnchantGUI").getLogger().warning(String.format("\u001B[33m DEBUG %s \u001B[0m",msg));
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            getLogger().severe("could not find vault");
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            getLogger().severe("could not find Economy.class");
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
     }
 
     public static Main getInstance() {
