aea0c3c66cff1e95423ba94e41c1ff4bac1bfa37
Omer Oreg <sarhatabaot1@gmail.com>
2019-07-04 20:17:16 +0300

    Code cleanup

--- src/main/java/me/tychsen/enchantgui/menu/DefaultMenuGenerator.java
+++ src/main/java/me/tychsen/enchantgui/menu/DefaultMenuGenerator.java
@@ -1,5 +1,6 @@
 package me.tychsen.enchantgui.menu;
 
+import lombok.AllArgsConstructor;
 import me.tychsen.enchantgui.config.EShopConfig;
 import me.tychsen.enchantgui.config.EShopEnchants;
 import me.tychsen.enchantgui.config.EShopShop;
@@ -20,6 +21,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static me.tychsen.enchantgui.config.EShopConfig.*;
+
 //TODO: Cache itemstacks
 public class DefaultMenuGenerator implements MenuGenerator {
     private int inventorySize;
@@ -32,11 +35,10 @@ public class DefaultMenuGenerator implements MenuGenerator {
         this.config = config;
         this.enchants = new EShopEnchants();
         this.permSys = permSys;
-
     }
 
     public Inventory mainMenu(@NotNull Player p) {
-        Inventory inv = p.getServer().createInventory(p, inventorySize, config.getMenuName());
+        Inventory inv = p.getServer().createInventory(p, inventorySize, getMenuName());
         generateMainMenu(p, inv);
 
         return inv;
@@ -62,14 +64,14 @@ public class DefaultMenuGenerator implements MenuGenerator {
 
         for (ItemStack item : enchantList) {
             if (permSys.hasEnchantPermission(p, item)) {
-                if (config.getShowPerItem()) {
+                if (getShowPerItem()) {
                     itemList = showPerItem(itemList, item, p);
                 } else {
                     itemList.add(item);
                 }
             }
         }
-        inv.setContents(itemList.toArray(new ItemStack[itemList.size()]));
+        inv.setContents(itemList.toArray(new ItemStack[0]));
     }
 
     @NotNull
@@ -81,29 +83,28 @@ public class DefaultMenuGenerator implements MenuGenerator {
     }
 
     private ItemStack generateItemWithMeta(@NotNull ItemStack item, int level, Enchantment enchantment) {
-        ItemStack tmp = item.clone();
-        ItemMeta meta = tmp.getItemMeta();
+        ItemStack temp = item.clone();
+        ItemMeta meta = temp.getItemMeta();
         List<String> lores = new ArrayList<>();
         lores.add(format(level, "level"));
 
         if (!(config.getEconomy() instanceof NullPayment)) {
-            int price = config.getPrice(enchantment, level);
-            lores.add(format(price, "price"));
+            lores.add(format(getPrice(enchantment, level), "price"));
         }
 
         meta.setLore(lores);
-        tmp.setItemMeta(meta);
-        return tmp;
+        temp.setItemMeta(meta);
+        return temp;
     }
 
     private Inventory generateEnchantMenu(@NotNull Player p, @NotNull ItemStack item, Map<String, String[]> playerLevels) {
-        Inventory inv = p.getServer().createInventory(p, inventorySize, config.getMenuName());
+        Inventory inv = p.getServer().createInventory(p, inventorySize, getMenuName());
         Enchantment enchantment = item.getEnchantments().keySet().toArray(new Enchantment[1])[0];
         List<ItemStack> itemList = new ArrayList<>();
 
         // Generate the correct items for the player.
         // Based on permissions or OP status.
-        String[] enchantLevels = config.getEnchantLevels(enchantment);
+        String[] enchantLevels = getEnchantLevels(enchantment);
         List<String> levels = new ArrayList<>();
 
         for (String enchantLevel : enchantLevels) {
@@ -119,12 +120,12 @@ public class DefaultMenuGenerator implements MenuGenerator {
         }
 
         // Put the generated item list in the inventory
-        inv.setContents(itemList.toArray(new ItemStack[itemList.size()]));
+        inv.setContents(itemList.toArray(new ItemStack[0]));
 
         // Generate and insert a back button
         inv.setItem(27, generateBackItem());
 
-        playerLevels.put(p.getName(), levels.toArray(new String[levels.size()]));
+        playerLevels.put(p.getName(), levels.toArray(new String[0]));
         return inv;
     }
 
