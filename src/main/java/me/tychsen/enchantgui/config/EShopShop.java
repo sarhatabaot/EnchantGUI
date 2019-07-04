c2595845d6e6e1a8bc9f5be1dcb9acb37a6fd922
Omer Oreg <sarhatabaot1@gmail.com>
2019-07-04 20:18:22 +0300

    Added AConfig
    * Abstract class to handle config methods.
    * Removes duplicated code from LocalilizationManager and EShopShop

--- src/main/java/me/tychsen/enchantgui/config/EShopShop.java
+++ src/main/java/me/tychsen/enchantgui/config/EShopShop.java
@@ -2,69 +2,26 @@ package me.tychsen.enchantgui.config;
 
 import me.tychsen.enchantgui.Main;
 import me.tychsen.enchantgui.localization.LocalizationManager;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.Plugin;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
 
 /**
  * @author sarhatabaot
  */
-public class EShopShop {
-    private static final String FILE_NAME_SHOP = "shop.yml";
+public class EShopShop extends AConfig{
     private static EShopShop instance;
-    private Plugin plugin;
-    private File configFile;
-    private FileConfiguration config;
 
     private EShopShop(){
-        plugin = Main.getInstance();
+        super("shop.yml", Main.getInstance());
         saveDefaultConfiguration();
     }
 
     public void reload(CommandSender sender) {
         reloadConfig();
-        String start = ChatColor.AQUA + LocalizationManager.getInstance().getString("prefix") + " " + ChatColor.WHITE;
-        sender.sendMessage(start + LocalizationManager.getInstance().getString("shop-reloaded"));
+        sender.sendMessage(LocalizationManager.getInstance().getString("prefix") +" "+ LocalizationManager.getInstance().getString("shop-reloaded"));
     }
 
     public String getString(String string){
         return getConfig().getString(string);
-    }
-
-    private void saveDefaultConfiguration(){
-        if(configFile == null){
-            configFile = new File(plugin.getDataFolder(),FILE_NAME_SHOP);
-        }
-        if(!configFile.exists()){
-            plugin.saveResource(FILE_NAME_SHOP,false);
-        }
-    }
-
-    private void reloadConfig(){
-        if(configFile == null){
-            configFile = new File(plugin.getDataFolder(),FILE_NAME_SHOP);
-        }
-        config = YamlConfiguration.loadConfiguration(configFile);
-        Reader defaultConfigStream;
-        InputStream defaultConfigInputStream = plugin.getResource(FILE_NAME_SHOP);
-        if (defaultConfigInputStream != null) {
-            defaultConfigStream = new InputStreamReader(defaultConfigInputStream, StandardCharsets.UTF_8);
-            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(defaultConfigStream);
-            config.setDefaults(defaultConfig);
-        }
-        Main.debug("Config reloaded.");
-    }
-
-
-    private FileConfiguration getConfig(){
-        if(config==null)
-            reloadConfig();
-        return config;
     }
 
     public static EShopShop getInstance(){
--- src/main/java/me/tychsen/enchantgui/localization/LocalizationManager.java
+++ src/main/java/me/tychsen/enchantgui/localization/LocalizationManager.java
@@ -1,8 +1,8 @@
 package me.tychsen.enchantgui.localization;
 
 import me.tychsen.enchantgui.Main;
+import me.tychsen.enchantgui.config.AConfig;
 import me.tychsen.enchantgui.util.Common;
-import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -11,16 +11,11 @@ import org.bukkit.plugin.Plugin;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
-public class LocalizationManager {
-    private static final String FILE_NAME_LOCALIZATION = "localization.yml";
+public class LocalizationManager extends AConfig {
     private static LocalizationManager instance;
-    private Plugin plugin;
-    private File configFile;
-    private FileConfiguration config;
 
     private LocalizationManager() {
-        plugin = Main.getInstance();
-
+        super("localization.yml", Main.getInstance());
         saveDefaultConfiguration();
     }
 
@@ -30,40 +25,7 @@ public class LocalizationManager {
 
     public void reload(CommandSender sender) {
         reloadConfig();
-        String start = ChatColor.AQUA + LocalizationManager.getInstance().getString("prefix") + " " + ChatColor.WHITE;
-        sender.sendMessage(start + LocalizationManager.getInstance().getString("localization-reloaded"));
-    }
-
-    private void saveDefaultConfiguration() {
-        if (configFile == null) {
-            configFile = new File(plugin.getDataFolder(), FILE_NAME_LOCALIZATION);
-        }
-        if (!configFile.exists()) {
-            plugin.saveResource(FILE_NAME_LOCALIZATION, false);
-        }
-    }
-
-    private FileConfiguration getConfig() {
-        if (config == null) {
-            reloadConfig();
-        }
-        return config;
-    }
-
-    private void reloadConfig() {
-        if (configFile == null) {
-            configFile = new File(plugin.getDataFolder(), FILE_NAME_LOCALIZATION);
-        }
-        config = YamlConfiguration.loadConfiguration(configFile);
-
-        // Look for defaults in the jar
-        Reader defaultConfigStream;
-        InputStream defaultConfigInputStream = plugin.getResource(FILE_NAME_LOCALIZATION);
-        if (defaultConfigInputStream != null) {
-            defaultConfigStream = new InputStreamReader(defaultConfigInputStream, StandardCharsets.UTF_8);
-            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(defaultConfigStream);
-            config.setDefaults(defaultConfig);
-        }
+        Common.tell(sender,instance.getString("prefix") +" "+ instance.getString("localization-reloaded"));
     }
 
     /**
